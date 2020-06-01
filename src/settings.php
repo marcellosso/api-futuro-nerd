c5bb68b00669ac5c4214d8658bcecff24a4f6943
Marcel Losso Forte <marcel.losso@gmail.com>
2020-06-01 16:40:25 -0300

    Changed require in settings

--- src/settings.php
+++ src/settings.php
@@ -1,5 +1,10 @@
 <?php
-require_once('../../admin/config.php');
+//require_once('../../admin/config.php');
+
+define('URL_API_FUTURONERD',    'http://localhost:8080/');
+define('URL_ADMIN_FUTURONERD',  'http://localhost:8081/');
+define('URL_CONSULTA_CEP',      'https://viacep.com.br/ws/');
+
 return [
     'settings' => [
         'displayErrorDetails'    => true, // set to false in production
