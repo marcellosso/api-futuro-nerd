711aae76f5cd9cb473155b70629803744334b4c9
Marcel Losso Forte <marcel.losso@gmail.com>
2020-06-01 17:03:27 -0300

    Changed db settings

--- src/settings.php
+++ src/settings.php
@@ -1,7 +1,7 @@
 <?php
 //require_once('../../admin/config.php');
 
-define('URL_API_FUTURONERD',    'http://localhost:8080/');
+define('URL_API_FUTURONERD',    'https://api-futuronerd.herokuapp.com/');
 define('URL_ADMIN_FUTURONERD',  'http://localhost:8081/');
 define('URL_CONSULTA_CEP',      'https://viacep.com.br/ws/');
 
