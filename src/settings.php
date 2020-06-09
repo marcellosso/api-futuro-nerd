f8fb12394750b001e90e2d84d2ec521ed17457de
Marcel Losso Forte <marcel.losso@gmail.com>
2020-06-08 23:47:59 -0300

    Changed settings files to point to google cloud db

--- src/settings.php
+++ src/settings.php
@@ -26,11 +26,18 @@ return [
 //            'dbname'   => 'futuro_nerd_local' //'dbname'   => 'futuronerd',
 //        ],
 
+//        'db' => [
+//            'host'     => 'ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//            'user'     => 'uvmrvwcmw19kt2su',
+//            'password' => 'zawuvgqtjcs5t9ff',
+//            'dbname'   => 'p040hst38t6dnx4w'
+//        ],
+
         'db' => [
-            'host'     => 'ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-            'user'     => 'uvmrvwcmw19kt2su',
-            'password' => 'zawuvgqtjcs5t9ff',
-            'dbname'   => 'p040hst38t6dnx4w'
+            'host'     => '35.223.255.249',
+            'user'     => 'admin',
+            'password' => 'admin@123',
+            'dbname'   => 'futuronerd'
         ],
 
         // Monolog settings
